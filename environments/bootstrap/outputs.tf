31ad9321133feb22d9637aba1268a806b2967ab2
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2026-05-28 17:03:24 -0300

    atualiza bootstrap com output do bucket e instrucoes

--- environments/bootstrap/outputs.tf
+++ environments/bootstrap/outputs.tf
@@ -1,3 +1,12 @@
 output "bucket_name" {
   value = aws_s3_bucket.state.bucket
 }
+
+output "init_dev" {
+  value = <<-EOT
+    Execute no dev:
+      cd environments/dev
+      terraform init -reconfigure
+      terraform apply
+  EOT
+}
