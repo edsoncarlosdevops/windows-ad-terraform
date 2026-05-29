2e39ba31103ec4cdbf46b09cb9d0741a8ff15993
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2026-05-29 15:20:02 -0300

    fix: restore http provider in dev provider.tf

---
+++ environments/dev/provider.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-windows-ad-87ryrbcc"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
